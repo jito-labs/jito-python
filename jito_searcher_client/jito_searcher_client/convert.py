8e62eb7890b1f2e77057fc07512f441d3e5331d0
frank <98238480+soundsonacid@users.noreply.github.com>
2024-03-26 10:14:13 -0500

    update dependencies & tx to packet fns  (#5)

--- jito_searcher_client/jito_searcher_client/convert.py
+++ jito_searcher_client/jito_searcher_client/convert.py
@@ -1,6 +1,18 @@
-from solders.transaction import Transaction
+from solana.transaction import Transaction
+from solders.transaction import VersionedTransaction
 
 from jito_searcher_client.generated.packet_pb2 import Meta, Packet
+
+
+def versioned_tx_to_protobuf_packet(tx: VersionedTransaction) -> Packet:
+    """
+    Converts a versioned transaction to a packet
+    Note: setting packet.meta.size is required, the rest are optional
+    """
+    return Packet(
+        data=bytes(tx),
+        meta=Meta(size=len(bytes(tx)), addr="0.0.0.0", port=0, flags=None, sender_stake=0),
+    )
 
 
 def tx_to_protobuf_packet(tx: Transaction) -> Packet:
@@ -8,4 +20,7 @@ def tx_to_protobuf_packet(tx: Transaction) -> Packet:
     Converts a transaction to a packet
     Note: setting packet.meta.size is required, the rest are optional
     """
-    return Packet(data=bytes(tx), meta=Meta(size=len(bytes(tx)), addr="0.0.0.0", port=0, flags=None, sender_stake=0))
+    return Packet(
+        data=tx.serialize(),
+        meta=Meta(size=len(tx.serialize()), addr="0.0.0.0", port=0, flags=None, sender_stake=0),
+    )
